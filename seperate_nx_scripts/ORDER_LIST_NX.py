a48f7dbd844d6e116b008249b676cfb4ce145668
Denis Artemov <denis.artemov.k40@gmail.com>
2021-02-26 16:39:25 +0300

    git commit -m "Second Commit"

--- seperate_nx_scripts/ORDER_LIST_NX.py
+++ seperate_nx_scripts/ORDER_LIST_NX.py
@@ -7,6 +7,7 @@ import NXOpen.CAM
 import json
 
 def main():
+    
     theSession = NXOpen.Session.GetSession()
     theSession.Parts.LoadOptions.UsePartialLoading = False
     displayPart = theSession.Parts.Display
@@ -14,38 +15,19 @@ def main():
     camSession = theSession.CreateCamSession()
     start_point = workPart.CAMSetup.CAMOperationCollection.FindObject("GEOMETRY")
     objects_at_start = NXOpen.CAM.NCGroup.GetMembers(start_point)
+    op_data_list = _operations_collector(objects_at_start, workPart)
+
+    # Create Json file
+    with open(r"C:\Users\vyacheslav\Desktop\FBM\feature_recognition_machining-master\output\ordering_tests.txt", 'w') as result_file:
+        json.dump(op_data_list, result_file)
+
+def _operations_collector(objects_at_start, workPart):
+    
     group_list = []
     checked_list = []
     operation_list = []
-    
-    def _get_op_data(obj):
-        op_data = {}
-        op_data['Operation name'] = obj.Name
-        cutting_tool = obj.GetParent(NXOpen.CAM.CAMSetup.View.MachineTool)
-        op_data['Tool'] = cutting_tool.Name
-        op_data['Stock value'] = str(obj.GetRealValue("Stock Part"))
-        op_data['Spindle RPM'] = str(obj.GetRealValue("Spindle RPM"))
-        #op_data['Depth'] = str(obj.GetRealValue("Depth Per Cut"))
-        #op_data['Depth Per Cut'] = str(obj.GetRealValue("Depth Per Cut"))
-        #_print(str(obj.GetRealValue("Depth Per Cut")))
-        #_print(str(obj))
-        feed_rate_tuple = obj.GetFeedRate("Feed Cut")
-        if feed_rate_tuple[0] == NXOpen.CAM.CAMObject.FeedRateUnit.PerMinute:
-            feed_rate_units = "mm/min"
-        elif feed_rate_tuple[0] == NXOpen.CAM.CAMObject.FeedRateUnit.PerRevolution:
-            feed_rate_units = "mm/rev"
-        else:
-            feed_rate_units = "None"
-        op_data['Feedrate value'] = str(feed_rate_tuple[1])    
-        op_data['Feedrate type'] = feed_rate_units
-        op_data['Path time'] = str(obj.GetToolpathTime())
-        op_data['Toolpath Cutting Length'] = str(obj.GetToolpathCuttingLength()) # Returns toolpath cutting length in Part units (mm or inch)
-        parent_feature = obj.GetParent(NXOpen.CAM.CAMSetup.View.Geometry) # parent feature group
-        orient = parent_feature.GetParent()
-        op_data['MCS'] = str(orient.Name)
-        return op_data
-
     op_data_list = []
+
     for each_object in objects_at_start:
         if workPart.CAMSetup.IsGroup(each_object) == True:
             group_list.append(each_object.Name)
@@ -64,10 +46,36 @@ def main():
                         op_data = _get_op_data(sub_object)
                         op_data_list.append(op_data)
                 checked_list.append(each_object)
-    #_print(str(op_data_list))
-    # Create Json file
-    with open(r"C:\Users\vyacheslav\Desktop\FBM\feature_recognition_machining-master\output\ordering_tests.txt", 'w') as result_file:
-        json.dump(op_data_list, result_file)
+    return(op_data_list)
+
+def _get_op_data(obj):
+    op_data = {}
+    op_data['Operation name'] = obj.Name
+    cutting_tool = obj.GetParent(NXOpen.CAM.CAMSetup.View.MachineTool)
+    op_data['Tool'] = cutting_tool.Name
+    op_data['Tool Description'] = str(cutting_tool.GetStringValue("Template Subtype"))
+    op_data['Stock value'] = str(obj.GetRealValue("Stock Part"))
+    op_data['Spindle RPM'] = str(obj.GetRealValue("Spindle RPM"))
+    if "25DMilling"  in str(obj):
+        op_data['Depth Per Cut'] = str(obj.GetRealValue("Depth Per Cut"))
+    else:
+        op_data['Depth Per Cut'] = str("-")
+    feed_rate_tuple = obj.GetFeedRate("Feed Cut")
+    if feed_rate_tuple[0] == NXOpen.CAM.CAMObject.FeedRateUnit.PerMinute:
+        feed_rate_units = "mm/min"
+    elif feed_rate_tuple[0] == NXOpen.CAM.CAMObject.FeedRateUnit.PerRevolution:
+        feed_rate_units = "mm/rev"
+    else:
+        feed_rate_units = "None"
+    op_data['Feedrate value'] = str(feed_rate_tuple[1])    
+    op_data['Feedrate type'] = feed_rate_units
+    op_data['Path time'] = str(obj.GetToolpathTime())
+    op_data['Toolpath Cutting Length'] = str(obj.GetToolpathCuttingLength()) # Returns toolpath cutting length in Part units (mm or inch)
+    parent_feature = obj.GetParent(NXOpen.CAM.CAMSetup.View.Geometry) # parent feature group
+    orient = parent_feature.GetParent()
+    op_data['MCS'] = str(orient.Name)
+    op_data['Operation Description'] = str(obj.GetStringValue("Template Subtype"))
+    return op_data
 
 def _print(message):
     ses_ui = NXOpen.UI.GetUI()
--- seperate_nx_scripts/sort_test.py
+++ seperate_nx_scripts/sort_test.py
@@ -9,9 +9,20 @@ Order_data = r"C:\Users\vyacheslav\Desktop\FBM\feature_recognition_machining-mas
 with open(Order_data) as json_file:
     data= json.load(json_file)
 
+key_value ={} 
+
+# Initializing the value  
+key_value[1] = "MCS_TOP"      
+key_value[2] = "MCS_DOWN"
+key_value[3] = "MCS_FRONT"      
+key_value[4] = "MCS_BACK" 
+key_value[5] = "MCS_LEFT" 
+key_value[6] = "MCS_RIGHT"
+
+print(data[0])
 
 #print(data)
 #sorted(data, key="MCS_TOP")
 
-df_json = pd.read_json(Order_data)
-df_json.to_excel(r"C:\Users\vyacheslav\Desktop\FBM\feature_recognition_machining-master\output\ordering_tests.xlsx", sheet_name='Operations List', index=False)
+#df_json = pd.read_json(Order_data)
+#df_json.to_excel(r"C:\Users\vyacheslav\Desktop\FBM\feature_recognition_machining-master\output\ordering_tests.xlsx", sheet_name='Operations List', index=False)
